0e909017f2f73e3a5a362d22088676437e864d9e
Alex <aeversme@gmail.com>
2021-03-14 19:20:08 -0400

    add flask-bootstrap to template

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from dotenv import load_dotenv
+from datetime import date
 import os
 
 app = Flask(__name__)
+Bootstrap(app)
 load_dotenv()
+current_year = date.today().year
 
 app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY')
 
 
 @app.route('/')
 def get_index():
-    return render_template('index.html')
+    return render_template('index.html', year=current_year)
 
 
 if __name__ == '__main__':
