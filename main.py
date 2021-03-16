2cb81b9c884aff55c7e602ce3ccf4db4d69988fd
Alex <aeversme@gmail.com>
2021-03-16 14:31:57 -0400

    add import statements & set up forms and DB

--- main.py
+++ main.py
@@ -1,15 +1,92 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_ckeditor import CKEditor
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from sqlalchemy import ForeignKey
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import relationship
+from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+from flask_gravatar import Gravatar
+from functools import wraps
+import smtplib
 from dotenv import load_dotenv
 from datetime import date
 import os
 
 app = Flask(__name__)
-
-load_dotenv()
 current_year = date.today().year
+ckeditor = CKEditor(app)
+load_dotenv()
 
+# App & DB Config
 app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///blog.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+gravatar = Gravatar(app,
+                    size=100,
+                    rating='g',
+                    default='identicon',
+                    force_default=False,
+                    force_lower=False,
+                    use_ssl=False,
+                    base_url=None)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+def admin_only(function):
+    @wraps(function)
+    def check_user_id(*args, **kwargs):
+        if current_user.id != 1:
+            abort(403)
+        return function(*args, **kwargs)
+    return check_user_id
+
+
+# CONFIGURE TABLE
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    posts = relationship('BlogPost', back_populates='author')
+    comments = relationship('Comment', back_populates='comment_author')
+
+
+class BlogPost(db.Model):
+    __tablename__ = 'blog_posts'
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, ForeignKey('users.id'))
+    author = relationship('User', back_populates='posts')
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+    comments = relationship('Comment', back_populates='parent_post')
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, ForeignKey('users.id'))
+    comment_author = relationship('User', back_populates='comments')
+    post_id = db.Column(db.Integer, ForeignKey('blog_posts.id'))
+    parent_post = relationship('BlogPost', back_populates='comments')
+    text = db.Column(db.String(1000), nullable=False)
+
+
+db.create_all()
 
 
 @app.route('/')
